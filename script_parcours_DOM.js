2fe51e4126ef447afd0330af9df9b841ffc60b36
amine91480 <amine91480@hotmail.fr>
2020-02-29 14:26:21 +0100

    Test qui m'arche po

--- script_parcours_DOM.js
+++ script_parcours_DOM.js
@@ -1,1 +1,6 @@
-console.log(document.getElementsByTagName("a")[2]);
+// QUESTION 1 -> MArche
+console.log(document.getElementsByTagName("p").length);
+// QUESTION 2 -> MArche pas -> Undefined
+console.log(document.getElementById("coucou"));
+// QUESTION 3 ->Marche pas 
+console.log(document.getElementsByTagName("a")[2].href);
