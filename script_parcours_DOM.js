b26c80e813bdc120b3eee144739ca161194f1025
amine91480 <amine91480@hotmail.fr>
2020-02-29 15:30:26 +0100

    Réussite des 7 exo de la partie 1 -> OK

--- script_parcours_DOM.js
+++ script_parcours_DOM.js
@@ -1,6 +1,21 @@
-// QUESTION 1 -> MArche
+//1 --> Combien y a-t-il d'éléments <p> présents dans la page HTML ? Imprime-le résultat dans la console.
 console.log(document.getElementsByTagName("p").length);
-// QUESTION 2 -> MArche pas -> Undefined
-console.log(document.getElementById("coucou"));
-// QUESTION 3 ->Marche pas 
+
+//2 --> Quel est le contenu texte de l'élément portant l'id coucou ? Imprime-le dans la console.
+console.log(document.getElementById("coucou").innerHTML);
+
+//3 --> Quelle est l'URL vers laquelle pointe le 3ème élément <a> de la page HTML ? Imprime-la dans la console.
 console.log(document.getElementsByTagName("a")[2].href);
+
+//4 -->Combien d'éléments portent la classe compte-moi ? Imprime le résultat dans la console.
+console.log(document.getElementsByClassName('compte-moi').length)
+
+//5 --> Combien d'éléments <li> portent la classe compte-moi ? Imprime le résultat dans la console.
+console.log(document.querySelectorAll("li.compte-moi").length);
+
+//6 --> Combien d'éléments <li> et situés dans une liste ordonnée portent la classe compte-moi ? Imprime le résultat dans la console.
+console.log(document.querySelectorAll("ol > li.compte-moi").length);
+
+//7 --> Petit jeu de piste à résoudre si possible sans regarder le fichier HTML…
+//La page contient un seul élément <div>. Celui-ci contient 2 éléments "unordered list" ou <ul>. Dans le second <ul>, le premier élément de la liste (tag <li>) est caché visuellement de l'utilisateur mais toi, tu peux en récupérer le contenu. Affiche-le dans la console.
+console.log(document.querySelectorAll('div > ul ')[1].children[0].innerHTML)
